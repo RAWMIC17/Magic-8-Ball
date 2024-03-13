531f4f3cf84aecff3212e4b9bd9a49a5fc310322
RAWMIC17 <hks1710harsh@gmail.com>
2024-03-14 00:15:01 +0530

    day1 completed

--- lib/ball.dart
+++ lib/ball.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 
@@ -8,19 +9,30 @@ class BallPage extends StatefulWidget {
   State<BallPage> createState() => _BallPageState();
 }
 
-int ballnumber = 1;
-
-void changeball(){
-  //setstate((){})
-}
-
 class _BallPageState extends State<BallPage> {
+  int ballnumber = 1;
+
+  void changeball() {
+    setState(() {
+        ballnumber = Random().nextInt(5) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Vx.blue300,
-        body: Image.asset(""),
+        body: Center(
+            child: TextButton(
+          child: Image.asset("lib/assets/images/ball$ballnumber.png"),
+          style: ButtonStyle(splashFactory: NoSplash.splashFactory),
+          onPressed: () {
+            changeball();
+          },
+          
+        )
+        ),
       ),
     );
   }
